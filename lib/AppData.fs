a91c8981c6d02b431972765f19f63e01bc8dea18
Alfredo Pironti <alfredo.pironti@inria.fr>
2011-02-16 16:32:51 +0000

    Make AppData "fragment-aware"

--- lib/AppData.fs
+++ lib/AppData.fs
@@ -8,24 +8,53 @@ open Formats
 
 type app_state = {
   app_info: SessionInfo
+  app_outgone: bytes
+  app_out_fragno: int
+
   app_incoming: bytes (* unsolicited data *)
   app_outgoing: bytes (* empty_bstr if nothing to be sent *) 
-  (* we are still unsure what to do when we CCS with incoming/outgoing data *)
 }
 
-let init info = {app_info = info ; app_incoming = empty_bstr; app_outgoing = empty_bstr}
+let init info =
+    {app_info = info;
+     app_outgone = empty_bstr;
+     app_out_fragno = 0;
+     app_incoming = empty_bstr;
+     app_outgoing = empty_bstr}
   
 let send_data (state:app_state) (data:bytes) =
-    correct (state)
+    let new_out = append state.app_outgoing data in
+    {state with app_outgoing = new_out}
+
+let retrieve_data (state:app_state) (len:int) :(bytes * app_state) option =
+    None
+    (*
+    if equalBytes state.app_incoming empty_bstr then
+        None
+    else
+        let (f,rem) = split state.app_incoming len in
+        let state = {state with app_incoming = rem} in
+        let res = (f,state) in
+        Some res
+    *)
+
 
 let next_fragment state len =
     if equalBytes state.app_outgoing empty_bstr then
         None
     else
         let (f,rem) = split state.app_outgoing len in
-        let state = {state with app_outgoing = rem} in
+        let new_fragno = state.app_out_fragno + 1 in (* GHOST *)
+        let new_outgone = append state.app_outgone f in (* GHOST *)
+        let state = {state with app_outgoing = rem
+                                app_out_fragno = new_fragno (* GHOST *)
+                                app_outgone = new_outgone} in (* GHOST *)
         let res = (f,state) in
         Some res
 
 let recv_fragment (state:app_state) (fragment:fragment) =
-    correct (state)
+    correct(state)
+    (*
+    let new_in = append state.app_incoming fragment in
+    correct ({state with app_incoming = new_in})
+    *)
--- lib/AppData.fsi
+++ lib/AppData.fsi
@@ -13,8 +13,17 @@ type app_state
 
 val init: SessionInfo -> app_state
 
-val send_data: app_state -> bytes -> app_state Result
+(* Application data to/form application *)
 
+(* Enqueue app data in the output buffer *)
+val send_data: app_state -> bytes -> app_state
+(* Dequeue app data from the input buffer *)
+val retrieve_data: app_state -> int -> (bytes * app_state) option
+
+
+(* Application data to/from dispatcher (hence record) *)
+
+(* Dequeue app data from the output buffer *)
 val next_fragment: app_state -> int -> (fragment * app_state) option
-
+(* Enqueue app data in the input buffer *)
 val recv_fragment: app_state -> fragment -> app_state Result 
