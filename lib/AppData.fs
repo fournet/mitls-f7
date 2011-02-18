901148d660d94d8a4882317a4528030a40a12955
Alfredo Pironti <alfredo.pironti@inria.fr>
2011-02-18 13:01:19 +0000

    backup commit: adding FIFOs; using them in appData; updating dispatcher with new predicates

--- lib/AppData.fs
+++ lib/AppData.fs
@@ -5,34 +5,33 @@ open Record
 open Error_handling
 open Sessions
 open Formats
+open FIFO
 
 type app_state = {
   app_info: SessionInfo
-  app_outgone: bytes
-  app_out_fragno: int
+  app_outgone: Fifo
 
-  app_incoming: bytes (* unsolicited data *)
-  app_outgoing: bytes (* empty_bstr if nothing to be sent *) 
+  app_incoming: Fifo (* unsolicited data *)
+  app_outgoing: Fifo (* empty_bstr if nothing to be sent *) 
 }
 
 let init info =
     {app_info = info;
-     app_outgone = empty_bstr;
-     app_out_fragno = 0;
-     app_incoming = empty_bstr;
-     app_outgoing = empty_bstr}
+     app_outgone = empty_Fifo;
+     app_incoming = empty_Fifo;
+     app_outgoing = empty_Fifo}
   
 let send_data (state:app_state) (data:bytes) =
-    let new_out = append state.app_outgoing data in
+    let new_out = enqueue_data state.app_outgoing data in
     {state with app_outgoing = new_out}
 
 let retrieve_data (state:app_state) (len:int) :(bytes * app_state) option =
     None
     (*
-    if equalBytes state.app_incoming empty_bstr then
+    if is_empty_Fifo state.app_incoming then
         None
     else
-        let (f,rem) = split state.app_incoming len in
+        let (f,rem) = dequeue_data state.app_incoming len in
         let state = {state with app_incoming = rem} in
         let res = (f,state) in
         Some res
@@ -40,14 +39,12 @@ let retrieve_data (state:app_state) (len:int) :(bytes * app_state) option =
 
 
 let next_fragment state len =
-    if equalBytes state.app_outgoing empty_bstr then
+    if is_empty_Fifo state.app_outgoing then
         None
     else
-        let (f,rem) = split state.app_outgoing len in
-        let new_fragno = state.app_out_fragno + 1 in (* GHOST *)
-        let new_outgone = append state.app_outgone f in (* GHOST *)
+        let (f,rem) = dequeue_fragment state.app_outgoing len in
+        let new_outgone = enqueue_fragment state.app_outgone f in (* GHOST *)
         let state = {state with app_outgoing = rem
-                                app_out_fragno = new_fragno (* GHOST *)
                                 app_outgone = new_outgone} in (* GHOST *)
         let res = (f,state) in
         Some res
@@ -55,6 +52,6 @@ let next_fragment state len =
 let recv_fragment (state:app_state) (fragment:fragment) =
     correct(state)
     (*
-    let new_in = append state.app_incoming fragment in
+    let new_in = enqueue_fragment state.app_incoming fragment in
     correct ({state with app_incoming = new_in})
     *)
---
+++ lib/FIFO.fs
@@ -0,0 +1,31 @@
+﻿module FIFO
+
+open Data
+open Record
+open Formats
+
+type Fifo = bytes
+
+
+type preds =
+    | FIFOSend of fragment
+
+let empty_Fifo = empty_bstr
+
+let is_empty_Fifo f =
+    equalBytes f empty_bstr
+
+let enqueue_data f d =
+    append f d
+
+let enqueue_fragment f d =
+    append f d
+
+let dequeue_data f len =
+    split f len
+
+let dequeue_fragment f len =
+    let (frag,fifo) = split f len in
+    Pi.assume(FIFOSend(frag));
+    (frag,fifo)
+    
