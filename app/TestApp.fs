bba04d24a9f435defaa5f13d8963565ba432c350
Alfredo Pironti <alfredo.pironti@inria.fr>
2011-03-09 11:36:03 +0000

    Really add the tentative TestApp

--- app/TestApp.fs
+++ app/TestApp.fs
@@ -3,7 +3,57 @@
 open Error_handling
 open AppCommon
 
-let _ =
-    let ns = Tcp.connect "alfredo.pironti.eu" 443 in
-    let state = TLS.connect ns defaultProtocolOptions in
-    ()
+let expected_req = System.Text.Encoding.ASCII.GetBytes("GET /secret.key")
+let canned_resp = System.Text.Encoding.ASCII.GetBytes("Some data")
+let secret_resp = System.Text.Encoding.ASCII.GetBytes("Some secret data")
+
+let clientAuthOps = defaultProtocolOptions (* Plus the option to require client auth... *)
+
+(* Checks whether the connected client is the one we are willing to talk to *)
+let valid_client_id tls = true
+
+let flawed =
+    let list = Tcp.listen "0.0.0.0" 80 in
+    match TLS.accept list defaultProtocolOptions with
+    | (Correct(x),tls) ->
+        match TLS.read tls 15 with
+        | (Correct(req),tls) ->
+            if req = expected_req then
+                match TLS.handshakeRequest_now tls clientAuthOps with
+                | (Correct(x),tls) ->
+                    if valid_client_id tls then
+                        ignore (TLS.writeFully tls secret_resp)
+                    else
+                        failwith "Invalid client ID"
+                | (Error(x,y),tls) -> failwith "Impossible to autenticate client"
+            else
+                (* Send canned response *)
+                ignore (TLS.writeFully tls canned_resp)
+        | (Error(x,y),tls) -> failwith "Cannot read from client"
+    | (Error(x,y),tls) -> failwith "Impossible to set up the TLS connection"
+
+let correct =
+    let list = Tcp.listen "0.0.0.0" 80 in
+    match TLS.accept list defaultProtocolOptions with
+    | (Correct(x),tls) ->
+        match TLS.read tls 15 with
+        | (Correct(req),tls) ->
+            if req = expected_req then
+                match TLS.handshakeRequest_now tls clientAuthOps with
+                | (Correct(x),tls) ->
+                    if valid_client_id tls then
+                        match TLS.read tls 15 with (* Re-read request from now-authenticated client *)
+                        | (Correct(req),tls) ->
+                            if req = expected_req then
+                                ignore (TLS.writeFully tls secret_resp)
+                            else
+                                failwith "Authenticated client did not confirm its request"
+                        | (Error(x,y),tls) -> failwith "Cannot read from client"
+                    else
+                        failwith "Invalid client ID"
+                | (Error(x,y),tls) -> failwith "Impossible to autenticate client"
+            else
+                (* Send canned response *)
+                ignore (TLS.writeFully tls canned_resp)
+        | (Error(x,y),tls) -> failwith "Cannot read from client"
+    | (Error(x,y),tls) -> failwith "Impossible to set up the TLS connection"
