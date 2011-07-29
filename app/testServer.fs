c322daa5ba5c8f869fe75c9b2c325381eb03dfd9
Alfredo Pironti <alfredo.pironti@inria.fr>
2011-07-29 17:16:14 +0000

    Improve output of test application

--- app/testServer.fs
+++ app/testServer.fs
@@ -11,7 +11,7 @@ open System.Security.Cryptography.X509Certificates
 let rec testS_int listn =
     match TLS.accept listn options with
     | (Error(x,y),_) -> printf "AYEEEE!!! %A %A" x y
-    | (Correct (_), conn) -> printf "OK; C to continue, everything else to abort"
+    | (Correct (_), conn) -> printfn "OK; C to continue, everything else to abort"
     let resp = System.Console.ReadLine() in
     if resp = "C" then
         testS_int listn
