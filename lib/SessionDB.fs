bd6d068062146180b5e6cdf273c23767acc7a3b7
Alfredo Pironti <alfredo.pironti@inria.fr>
2011-12-07 11:02:18 +0000

    Make SessionSB thread safe

--- lib/SessionDB.fs
+++ lib/SessionDB.fs
@@ -4,6 +4,12 @@ open TLSInfo
 open AppCommon
 open System.IO
 open System.Runtime.Serialization.Formatters.Binary
+open System.Threading
+
+(* Lock Policy:
+   - Never acquire lock in basic load/store functions.
+   - Upper level functions acquire read/upgradeable read/write lock as appropriate *)
+let DBLock = new ReaderWriterLockSlim()
 
 type StorableSession =
     {sinfo: SessionInfo
@@ -30,41 +36,60 @@ let store filename (map:Map<sessionID,(StorableSession * System.DateTime)>) =
     file.Close()
 
 let create poptions =
+    DBLock.EnterWriteLock()
     let map = Map.empty<sessionID,(StorableSession * System.DateTime)> in
     store poptions.sessionDBFileName map
+    DBLock.ExitWriteLock()
 
-let remove poptions key = 
+let remove poptions key =
+    DBLock.EnterWriteLock()
     let map = load poptions.sessionDBFileName in
     let map = Map.remove key map in
     store poptions.sessionDBFileName map
+    DBLock.ExitWriteLock()
 
-let select poptions key = 
+let select poptions key =
+    DBLock.EnterUpgradeableReadLock()
     let map = load poptions.sessionDBFileName in
     match Map.tryFind key map with
-    | None -> None
+    | None ->
+        DBLock.ExitUpgradeableReadLock()
+        None
     | Some (sinfo,ts) ->
         (* Check timestamp validity *)
         let expires = ts + poptions.sessionDBExpiry in
         let now = System.DateTime.Now in
         if expires > now then
+            DBLock.ExitUpgradeableReadLock()
             Some (sinfo)
         else
+            (* Remove will upgrade to the Write Lock *)
             remove poptions key
+            DBLock.ExitUpgradeableReadLock()
             None
 
 let insert poptions key value =
+    DBLock.EnterUpgradeableReadLock()
     let map = load poptions.sessionDBFileName in
     (* If the session is already in the store, don't do anything.
        However, do _not_ use select to check availavility, or an expired
        session will be removed by select, and then re-added by us.
        Make direct access to the map instead *)
     match Map.tryFind key map with
-    | Some (sinfo,ts) -> ()
+    | Some (sinfo,ts) ->
+        DBLock.ExitUpgradeableReadLock()
+        ()
     | None ->
+        DBLock.EnterWriteLock()
         let map = Map.add key (value,System.DateTime.Now) map in
         store poptions.sessionDBFileName map
+        DBLock.ExitWriteLock()
+        DBLock.ExitUpgradeableReadLock()
 
 let getAllStoredIDs poptions =
+    DBLock.EnterReadLock()
     let map = load poptions.sessionDBFileName in
     let mapList = Map.toList map in
-    List.map (fun (x,y) -> x) mapList
+    let res = List.map (fun (x,y) -> x) mapList in
+    DBLock.ExitReadLock()
+    res
