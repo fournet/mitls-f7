def02a8a8ba414ff0607a641c99bde293feda0d7
Karthikeyan BHARGAVAN <karthikeyan.bhargavan@inria.fr>
2012-01-30 11:36:28 +0000

    more tc

--- lib/MACKey.fs
+++ lib/MACKey.fs
@@ -5,8 +5,8 @@ open Algorithms
 open CipherSuites
 open TLSInfo
 
-type key = {bytes:bytes}
+type key = {k:bytes}
 
-let GEN (ki) = {bytes= mkRandom (macKeySize (macAlg_of_ciphersuite ki.sinfo.cipher_suite))}
-let COERCE (ki:KeyInfo) k = {bytes=k}
-let LEAK (ki:KeyInfo) {bytes=k} = k 
+let GEN (ki) = {k= mkRandom (macKeySize (macAlg_of_ciphersuite ki.sinfo.cipher_suite))}
+let COERCE (ki:KeyInfo) k = {k=k}
+let LEAK (ki:KeyInfo) {k=k} = k 
