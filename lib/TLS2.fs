d5fac6f2a18396caa6cbb6f7276a64c1132653f0
Pierre-Yves Strub <pierre-yves@strub.nu>
2012-02-10 15:13:23 +0000

    TLS: event-driven high-level interface

---
+++ lib/TLS2.fs
@@ -0,0 +1,46 @@
+﻿module TLS2
+
+open Bytes
+open Error
+open Dispatch
+open TLSInfo
+open Tcp
+open AppConfig
+
+type ioerror = ErrorCause * ErrorKind
+
+type iointerrupt =
+| IOIWarning     of alertDescription
+| IOIAuthRequest of Certificate.cert list
+| IOIAgain
+
+type 'a ioresult =
+| IOSuccess     of 'a
+| IOInterrupted of iointerrupt
+| IOError       of ioerror
+
+type ioresult_o = unit  ioresult
+type ioresult_i = bytes ioresult
+
+let getSessionInfo conn =
+    Dispatch.getSessionInfo conn
+
+let read     (c : Connection)             : _ * ioresult_i = (c, IOInterrupted IOIAgain)
+let write    (c : Connection) (b : bytes) : _ * ioresult_o = (c, IOInterrupted IOIAgain)
+let flush    (c : Connection)             : _ * ioresult_o = (c, IOInterrupted IOIAgain)
+let shutdown (c : Connection)             : _ * ioresult_o = (c, IOInterrupted IOIAgain)
+
+let connect (s : NetworkStream) (opt : protocolOptions) : Connection ioresult = IOInterrupted IOIAgain
+let resume  (s : NetworkStream) (opt : protocolOptions) : Connection ioresult = IOInterrupted IOIAgain
+
+let rehandshake     = fun (c : Connection) (opt : protocolOptions) -> c
+let rehandshake_now = fun (c : Connection) (opt : protocolOptions) -> (c, (IOInterrupted IOIAgain : ioresult_o))
+
+let rekey     = fun (c : Connection) (opt : protocolOptions) -> c
+let rekey_now = fun (c : Connection) (opt : protocolOptions) -> (c, (IOInterrupted IOIAgain : ioresult_o))
+
+let handshakeRequest     = fun (c : Connection) (opt : protocolOptions) -> c
+let handshakeRequest_now = fun (c : Connection) (opt : protocolOptions) -> (c, (IOInterrupted IOIAgain : ioresult_o))
+
+let accept            (s : TcpListener  ) (opt : protocolOptions) : Connection ioresult = IOInterrupted IOIAgain
+let accept_connected  (s : NetworkStream) (opt : protocolOptions) : Connection ioresult = IOInterrupted IOIAgain
---
+++ lib/TLS2.fsi
@@ -0,0 +1,47 @@
+﻿module TLS2
+
+open Bytes
+open Error
+open Dispatch
+open TLSInfo
+open Tcp
+open AppConfig
+
+(* Event-driven interface *)
+
+type ioerror = ErrorCause * ErrorKind
+
+type iointerrupt =
+| IOIWarning     of alertDescription
+| IOIAuthRequest of Certificate.cert list
+| IOIAgain
+
+type 'a ioresult =
+| IOSuccess     of 'a
+| IOInterrupted of iointerrupt
+| IOError       of ioerror
+
+type ioresult_o = unit  ioresult
+type ioresult_i = bytes ioresult
+
+val getSessionInfo: Connection -> SessionInfo
+
+val read     : Connection -> Connection * ioresult_i
+val write    : Connection -> bytes -> Connection * ioresult_o
+val flush    : Connection -> Connection * ioresult_o
+val shutdown : Connection -> Connection * ioresult_o
+
+val connect : NetworkStream -> protocolOptions -> Connection ioresult
+val resume  : NetworkStream -> protocolOptions -> Connection ioresult
+
+val rehandshake     : Connection -> protocolOptions -> Connection
+val rehandshake_now : Connection -> protocolOptions -> Connection * ioresult_o
+
+val rekey     : Connection -> protocolOptions -> Connection
+val rekey_now : Connection -> protocolOptions -> Connection * ioresult_o
+
+val handshakeRequest     : Connection -> protocolOptions -> Connection
+val handshakeRequest_now : Connection -> protocolOptions -> Connection * ioresult_o
+
+val accept           : TcpListener   -> protocolOptions -> Connection ioresult
+val accept_connected : NetworkStream -> protocolOptions -> Connection ioresult
