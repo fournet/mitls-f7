8d0bc2ca6ef6a0ad692e34b5d4957aca61780d6a
Pierre-Yves Strub <pierre-yves@strub.nu>
2012-04-30 14:43:06 +0000

    RPC lib

--- RPC/RPC.fs
+++ RPC/RPC.fs
@@ -1,76 +1,139 @@
 ﻿module RPC
 
 open Bytes
-open AppConfig
+open TLSInfo
 open Error
+open DataStream
+open Dispatch
 open TLS
 
 let config certname = {
-    AppConfig.minVer = CipherSuites.ProtocolVersion.SSL_3p0
-    AppConfig.maxVer = CipherSuites.ProtocolVersion.TLS_1p2
+    TLSInfo.minVer = CipherSuites.ProtocolVersion.SSL_3p0
+    TLSInfo.maxVer = CipherSuites.ProtocolVersion.TLS_1p2
 
-    AppConfig.ciphersuites =
+    TLSInfo.ciphersuites =
         CipherSuites.cipherSuites_of_nameList [
             CipherSuites.TLS_RSA_WITH_AES_128_CBC_SHA256;
             CipherSuites.TLS_RSA_WITH_AES_128_CBC_SHA;
             CipherSuites.TLS_RSA_WITH_3DES_EDE_CBC_SHA;
         ]
 
-    AppConfig.compressions = [ CipherSuites.NullCompression ]
+    TLSInfo.compressions = [ CipherSuites.NullCompression ]
 
     (* Client side *)
-    AppConfig.honourHelloReq = AppConfig.HRPResume
-    AppConfig.allowAnonCipherSuite = false
+    TLSInfo.honourHelloReq = TLSInfo.HRPResume
+    TLSInfo.allowAnonCipherSuite = false
 
     (* Server side *)
-    AppConfig.request_client_certificate = true
-    AppConfig.check_client_version_in_pms_for_old_tls = true
-    AppConfig.safe_renegotiation = true
+    TLSInfo.request_client_certificate = true
+    TLSInfo.check_client_version_in_pms_for_old_tls = true
+    TLSInfo.safe_renegotiation = true
 
-    AppConfig.server_cert_file = certname
-    AppConfig.certificateValidationPolicy = (fun _ -> true)
-    AppConfig.isCompatibleSession = (fun oldS newS -> oldS = newS)
+    TLSInfo.server_cert_file = certname
+    TLSInfo.certificateValidationPolicy = (fun _ -> true)
 
-    AppConfig.sessionDBFileName = "sessionDBFile.bin"
-    AppConfig.sessionDBExpiry = new System.TimeSpan(2,0,0,0) (* two days *)
+    TLSInfo.sessionDBFileName = "sessionDBFile.bin"
+    TLSInfo.sessionDBExpiry = Bytes.newTimeSpan 2 0 0 0 (* two days *)
 }
 
-let request_bytes  nonce r = nonce @| r
-let response_bytes nonce r = nonce @| r
+let msglen = 128
+
+let padmsg = fun r ->
+    if Bytes.length r > msglen then
+        fst (Bytes.split r msglen)
+    else
+        r @| (Bytes.createBytes (msglen - (Bytes.length r)) 0)
+
+let request_bytes  nonce r = nonce @| (padmsg r)
+let response_bytes nonce r = nonce @| (padmsg r)
 
 let service = fun r -> r
 
-let doclient request =
+type DrainResult =
+| DRError    of ioerror
+| DRClosed   of Tcp.NetworkStream
+| DRContinue of Connection
+
+let rec drainMeta = fun conn ->
+  match TLS.read conn with
+  | ReadError  e         -> DRError e
+  | Close      s         -> DRClosed s
+  | Fatal      e         -> DRError (EFatal e)
+  | Warning    (conn, _) -> DRContinue conn
+  | CertQuery  (conn, q) -> DRContinue (authorize conn q)
+  | Handshaken conn      -> DRContinue conn
+  | DontWrite  conn      -> drainMeta conn
+  | Read       (conn, _) ->
+        ignore (TLS.shutdown conn)
+        DRError (EInternal (Error.TLS, Error.InvalidState))
+
+let rec sendMsg = fun conn rg msg ->
+    match TLS.write conn (rg, msg) with
+    | WriteError    e                 -> None
+    | WriteComplete conn              -> Some conn
+    | WritePartial  (conn, (rg, msg)) -> sendMsg conn rg msg
+    | MustRead      conn              ->
+        match drainMeta conn with
+        | DRError    _    -> None
+        | DRClosed   _    -> None
+        | DRContinue conn -> sendMsg conn rg msg
+
+let recvMsg = fun conn ->
+    let rec doit = fun conn buffer ->
+        match TLS.read conn with
+          | ReadError  _              -> None
+          | Close      _              -> None
+          | Fatal      _              -> None
+          | Warning    (conn, _)      -> doit conn buffer
+          | CertQuery  (conn, q)      -> doit (authorize conn q) buffer
+          | Handshaken conn           -> doit conn buffer
+          | DontWrite  conn           -> doit conn buffer
+          | Read       (conn, (r, d)) ->
+                let ki     = TLS.getEpochIn  conn in
+                let s      = TLS.getInStream conn in
+                let buffer = buffer @| (DataStream.deltaRepr ki s r d) in
+
+                if Bytes.length buffer < 2+msglen then
+                    doit conn buffer
+                elif Bytes.length buffer > 2+msglen then
+                    ignore (TLS.shutdown conn); None
+                else
+                    Some (conn, buffer)
+                    
+    in
+        doit conn [||]
+
+let doclient (request : string) =
     let options = config "client" in
 
     SessionDB.create options;
 
     let ns      = Tcp.connect "127.0.0.1" 5000 in
     let conn    = TLS.connect ns options in
+    let nonce   = Bytes.mkRandom 2 in
+    let request = request_bytes nonce (Bytes.utf8 request) in
 
-    match conn with
-    | (Error(x, y), conn) -> None
-    | (Correct _, conn) ->
-        let nonce   = Bytes.mkRandom 2 in
-        let request = request_bytes nonce (Bytes.utf8 request) in
-        let conn    = TLS.write conn request in
+    let msg =
+        DataStream.createDelta
+            (TLS.getEpochOut conn) (TLS.getOutStream conn)
+            (0, Bytes.length request) request in
 
-        match TLS.flush conn with
-        | (Error(x, y), conn) -> None
-        | (Correct _, conn) -> 
+    match sendMsg conn (0, Bytes.length request) msg with
+    | Some conn ->
+        match recvMsg conn with
+        | None -> None
+        | Some (conn, response) ->
+            ignore (TLS.shutdown conn);
 
-            match TLS.read conn with
-            | (conn, Error(x, y)) -> None
-            | (conn, Correct response) ->
-                TLS.shutdown conn;
-                if Bytes.length response < 2 then
-                    None
-                else
-                    let rnonce, response = Bytes.split response 2 in
-                        if Bytes.equalBytes nonce rnonce then
-                            Some (Bytes.iutf8 response)
-                        else
-                            None
+            if Bytes.length response <> 2+msglen then
+                None
+            else
+                let rnonce, response = Bytes.split response 2 in
+                    if Bytes.equalBytes nonce rnonce then
+                        Some (Bytes.iutf8 response)
+                    else
+                        None
+    | None -> None
 
 let doserver () =
     let options = config "server" in
@@ -83,24 +146,26 @@ let doserver () =
         let client = Tcp.accept ns in
 
         let result =
-            match TLS.accept_connected client options with
-            | (Error(x, y), conn) -> false
-            | (Correct _, conn) ->
-                match TLS.read conn with
-                | (conn, Error(x, y)) -> false
-                | (conn, Correct request) ->
-                    if Bytes.length request < 2 then
-                        false
-                    else
-                        let nonce, request = Bytes.split request 2 in
-                        let response = service (Bytes.iutf8 request) in
-                        let response = response_bytes nonce (Bytes.utf8 response) in
+            let conn = TLS.accept_connected client options in
 
-                        let conn = TLS.write conn response in
+            match recvMsg conn with
+            | None -> false
+            | Some (conn, request) ->
+                if Bytes.length request < 2 then
+                    false
+                else
+                    let nonce, request = Bytes.split request 2 in
+                    let response = service (Bytes.iutf8 request) in
+                    let response = response_bytes nonce (Bytes.utf8 response) in
 
-                            match TLS.flush conn with
-                            | (Error(x, y), conn) -> false
-                            | (Correct _, conn) -> TLS.shutdown conn; true
+                    let msg =
+                        DataStream.createDelta
+                            (TLS.getEpochOut conn) (TLS.getOutStream conn)
+                            (0, Bytes.length response) response in
+
+                    match sendMsg conn (0, Bytes.length response) msg with
+                    | Some conn -> ignore (TLS.shutdown conn); true
+                    | None -> false
         in
             Tcp.close client; result
     in
