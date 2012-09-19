37c47f903a80b91affbba143a0f690ed8e83511d
Pierre-Yves Strub <pierre-yves@strub.nu>
2012-09-19 15:47:01 +0000

    TLS: make Sig.fs be valid F#

--- lib/Sig.fs
+++ lib/Sig.fs
@@ -2,10 +2,13 @@
 
 // see the .fs7 for comments and discussion
 
+open Bytes
+open Algorithms
+
 type sigAlg = 
-  | RSA //[8.1.1] RSA digital signatures are performed using PKCS #1 block type 1. 
-  | DSA 
-  | ECDSA
+  | SA_RSA //[8.1.1] RSA digital signatures are performed using PKCS #1 block type 1. 
+  | SA_DSA 
+  | SA_ECDSA
 
 type hashAlg = // annoyingly not the same as Algorithms.hashAlg
   | MD5    // 1
@@ -24,19 +27,17 @@ type sigv = bytes
 let defaultAlg cs =
   let a = 
     match cs with 
-    | RSA | DH_RSA | DHE_RSA | RSA_PSK (* | ECDH_RSA | ECDHE_RSA *) -> RSA
-    | DH_DSS | DHE_DSS                                              -> DSA
-  (*| ECDH_ECDSA | ECDHE_ECDSA                                      -> ECDSA *)
+    | RSA | DH_RSA | DHE_RSA (* | RSA_PSK | ECDH_RSA | ECDHE_RSA *) -> SA_RSA
+    | DH_DSS | DHE_DSS                                              -> SA_DSA
+  (*| ECDH_ECDSA | ECDHE_ECDSA                                      -> SA_ECDSA *)
   (a, SHA1)
 
-type skey 
+type skey = bytes
 type vkey = bytes 
 
 let Gen     (a:alg): vkey * skey = failwith "todo"
 let Sign    (a:alg) (sk:skey) (t:text) : sigv = failwith "todo"
-let Verify: (a:alg) (vk:vkey) (t:text) (v:sigv) : bool = failwith "todo"
+let Verify  (a:alg) (vk:vkey) (t:text) (v:sigv) : bool = failwith "todo"
 
 let PkBytes (a:alg) (vk:vkey) : bytes = failwith "todo"
 let SkBytes (a:alg) (sk:skey) : bytes = failwith "todo"
-
-
