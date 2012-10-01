df7b046e3c33e2dce1cfc17fdc0df810620c8c7d
Cedric Fournet <fournet@microsoft.com>
2012-10-01 10:02:29 +0000

    forgotten

---
+++ lib/RSAPlain.fs
@@ -0,0 +1,17 @@
+﻿module RSAPlain
+open Bytes
+open TLSInfo
+
+type repr = bytes
+type pms = repr
+
+let genPMS (id:SessionInfo) (vc:CipherSuites.ProtocolVersion) : pms = 
+    let verBytes = CipherSuites.versionBytes vc in
+    let rnd = mkRandom 46 in
+    let pms = verBytes @| rnd in
+    pms
+
+failwith "tmp"
+let coerce id x = x
+let leak id x = x
+
