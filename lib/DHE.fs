965d88ab63a3faaf83e7489f58b940cfa052b4f6
Pierre-Yves Strub <pierre-yves@strub.nu>
2012-10-10 15:48:21 +0000

    TLS: DHE key generation implemented.

--- lib/DHE.fs
+++ lib/DHE.fs
@@ -3,13 +3,38 @@
 open Bytes
 open TLSInfo
 
+open System
+
+open Org.BouncyCastle.Math
+open Org.BouncyCastle.Security
+open Org.BouncyCastle.Crypto.Generators
+open Org.BouncyCastle.Crypto.Parameters
+
 type g = bytes
 type p = bytes
-type x = {x:bytes}
+type x = { x : bytes }
 type y = bytes
-type pms = {pms:bytes}
 
-let genParams () : g * p = failwith "TODO"
-let genKey (g:g) (p:p) : x * y = failwith "TODO"
-let genPMS (si:SessionInfo) (g:g) (p:p) (x:x) (y:y) : pms = failwith "TODO"
-let leak (si:SessionInfo) pms = pms.pms
+type pms = { pms : bytes }
+
+let genParams () : g * p =
+    let random    = new SecureRandom() in
+    let generator = new DHParametersGenerator() in
+        generator.Init(1024, 80, random);
+        let dhparams = generator.GenerateParameters() in
+            (dhparams.G.ToByteArrayUnsigned(), dhparams.P.ToByteArrayUnsigned())
+
+let genKey (g : g) (p : p) : x * y =
+    let dhparams = new DHParameters(new BigInteger(1, p), new BigInteger(1, g)) in
+    let kparams  = new DHKeyGenerationParameters(new SecureRandom(), dhparams) in
+    let kgen     = new DHKeyPairGenerator() in
+        kgen.Init(kparams);
+        let kpair = kgen.GenerateKeyPair() in
+        let pkey  = (kpair.Public  :?> DHPublicKeyParameters ) in
+        let skey  = (kpair.Private :?> DHPrivateKeyParameters) in
+            ({ x = skey.X.ToByteArrayUnsigned() }, pkey.Y.ToByteArrayUnsigned())
+
+let genPMS (si:SessionInfo) (g : g) (p : p) (x : x) (y : y) : pms =
+    failwith "TODO"
+
+let leak (si : SessionInfo) pms = pms.pms
