f14fddab390eb43ac0723760d6cbf61b986778cb
Cedric Fournet <fournet@microsoft.com>
2012-10-16 15:14:01 +0000

    details

--- lib/MAC.fs
+++ lib/MAC.fs
@@ -4,7 +4,7 @@ open Bytes
 open Algorithms
 open CipherSuites
 open TLSInfo
-open HASH (* Only for SSL 3 keyed hash *)
+//open HASH (* Only for SSL 3 keyed hash *)
 open Error
 
 type text = bytes
@@ -19,17 +19,17 @@ let Mac ki key data =
     let pv = si.protocol_version in
     let a = macAlg_of_ciphersuite si.cipher_suite in
     match pv with
-    | SSL_3p0 when a = SHA || a = MD5 ->     HMAC.sslKeyedHash a key.k data 
-        (* KB: added a when clause above. Remove it when epoch guarantees the condition as an invariant. *)
-    | TLS_1p0 | TLS_1p1 | TLS_1p2 -> HMAC.HMAC a key.k data
+    (* KB: added a when clause below. Remove it when epoch guarantees the condition as an invariant. *)
+    | SSL_3p0 when a = SHA || a = MD5 -> HMAC.sslKeyedHash a key.k data 
+    | TLS_1p0 | TLS_1p1 | TLS_1p2     -> HMAC.HMAC         a key.k data
 
 let Verify ki key data tag =
     let si = epochSI(ki) in
     let pv = si.protocol_version in
     let a = macAlg_of_ciphersuite si.cipher_suite in
     match pv with
-    | SSL_3p0 when a = SHA || a = MD5 ->     HMAC.sslKeyedHashVerify a key.k data tag
-    | TLS_1p0 | TLS_1p1 | TLS_1p2 -> HMAC.HMACVERIFY a key.k data tag
+    | SSL_3p0 when a = SHA || a = MD5 -> HMAC.sslKeyedHashVerify a key.k data tag
+    | TLS_1p0 | TLS_1p1 | TLS_1p2     -> HMAC.HMACVERIFY         a key.k data tag
 
 let GEN (ki) =
     let si = epochSI(ki) in
