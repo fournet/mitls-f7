43e404e57b5bf5856508772633ebde550b2efb82
Cedric Fournet <fournet@microsoft.com>
2013-02-13 14:37:43 +0000

    details

--- lib/MAC.fs
+++ lib/MAC.fs
@@ -18,31 +18,33 @@ type key =
 // is ideal at Auth indexes; we do not need that assumption anymore, 
 // as we now typecheck this module against plain INT-CMA MAC interfaces:
 // idealization now occurs within each of their implementations.
-#if false
-type entry = epoch * text * tag
-let log:entry list ref=ref []
-let rec tmem (e:epoch) (t:text) (xs: entry list) = 
-  match xs with
-      [] -> false
-    | (e',t',tag)::res when e = e' && t = t' -> true
-    | (e',t',tag)::res -> tmem e t res
-#endif
+// 
+// #if ideal
+// type entry = epoch * text * tag
+// let log:entry list ref=ref []
+// let rec tmem (e:epoch) (t:text) (xs: entry list) = 
+//  match xs with
+//      [] -> false
+//    | (e',t',tag)::res when e = e' && t = t' -> true
+//    | (e',t',tag)::res -> tmem e t res
+// #endif
 
 let Mac ki key data =
     let si = epochSI(ki) in
     let a = macAlg_of_ciphersuite si.cipher_suite si.protocol_version in
-    #if false
-    let tag = 
-    #endif
+    // // Commented out old ideal specification:
+    // #if ideal
+    // let tag = 
+    // #endif
       match key with 
         | KeyNoAuth(k)  -> HMAC.MAC a k data 
         | Key_SHA256(k) -> MAC_SHA256.Mac ki k data 
         | Key_SHA1(k)   -> MAC_SHA1.Mac ki k data 
-    #if false
-    // We log every authenticated texts, with their index and resulting tag
-    log := (ki, data, tag)::!log;
-    tag
-    #endif
+    // #if ideal
+    // // We log every authenticated texts, with their index and resulting tag
+    // log := (ki, data, tag)::!log;
+    // tag
+    // #endif
 
 let Verify ki key data tag =
     let si = epochSI(ki) in
@@ -51,18 +53,20 @@ let Verify ki key data tag =
     | Key_SHA256(k) -> MAC_SHA256.Verify ki k data tag
     | Key_SHA1(k)   -> MAC_SHA1.Verify ki k data tag
     | KeyNoAuth(k) -> HMAC.MACVERIFY a k data tag
-    #if false
-    // At safe indexes, we use the log to detect and correct verification errors
-    && if safeMAC ki
-       then 
-           tmem ki data !log
-       else 
-           true  
-    #endif
+    // #if ideal 
+    // // At safe indexes, we use the log to detect and correct verification errors
+    // && if safeMAC ki
+    //   then 
+    //       tmem ki data !log
+    //   else 
+    //       true  
+    // #endif
 
 let GEN ki =
     let si = epochSI(ki) in
     #if ideal
+    // ideally, we separately keep track of "Auth" keys, 
+    // with an additional indirection to HMAFC  
     if safeMAC ki then 
       let a = macAlg_of_ciphersuite si.cipher_suite si.protocol_version in
       match a with 
