a0489b5dca715f63abad672d2cc0f019f30a3e88
Markulf Kohlweiss <markulf@microsoft.com>
2014-05-09 11:57:20 +0000

    Cert.tc7 verifies now, with some use of #if verif

--- lib/Cert.fs
+++ lib/Cert.fs
@@ -8,6 +8,7 @@ open Error
 open TLSError
 open UntrustedCert
 open CoreSig
+open RSAKey
 //open Sig
 
 type chain = UntrustedCert.chain
@@ -60,20 +61,22 @@ let for_key_encryption (sigkeyalgs : Sig.alg list) (h : hint) =
             match (find_enccert sigkeyalgs h) with 
             | Some(x509) ->                     
                 match x509_to_secret_key x509, x509_to_public_key x509 with
-                | Some (CoreSig.SK_RSA(sm, se)), Some(CoreSig.PK_RSA(pm,pe)) ->
+                | Some (CoreSig.SK_RSA(smse)), Some(CoreSig.PK_RSA(pmpe)) ->
+                    let (sm,se)=smse in
+                    let (pm,pe)=pmpe in
                     let chain = x509_chain x509 in
 
                     if forall (x509_check_key_sig_alg_one sigkeyalgs) chain then
-                        let pk = (RSAKey.create_rsapkey (pm,pe))
+                        let pk = RSAKey.create_rsapkey (pmpe) in
                         #if ideal
                         if RSAKey.honest pk then
                             None //loading of honest keys not implemented yet.
                         else
-                            let csk = (CoreACiphers.RSASKey(sm, se))
+                            let csk = CoreACiphers.RSASKey(smse)
                             let sk = RSAKey.coerce pk csk
                             Some (x509list_to_chain chain, sk)
                         #else
-                        Some (x509list_to_chain chain, RSAKey.coerce pk (CoreACiphers.RSASKey(sm, se)))
+                        Some (x509list_to_chain chain, RSAKey.coerce pk (CoreACiphers.RSASKey(smse)))
                         #endif
                     else
                         None
@@ -87,8 +90,8 @@ let get_public_signing_key (c : cert) ((siga, _) as a : Sig.alg) : Sig.pkey Resu
     | Some(x509) -> 
             if x509_is_for_signing x509 then
                 match siga, x509_to_public_key x509 with
-                | SA_RSA, Some (CoreSig.PK_RSA (sm, se) as k) -> Correct (Sig.create_pkey a k)
-                | SA_DSA, Some (CoreSig.PK_DSA (y, p  ) as k) -> Correct (Sig.create_pkey a k)
+                | SA_RSA, Some (k) -> Correct (Sig.create_pkey a k) //MK was Some (CoreSig.PK_RSA (sm, se) as k)
+                | SA_DSA, Some (k) -> Correct (Sig.create_pkey a k) //MK was Some (CoreSig.PK_DSA (y, p  ) as k)
                 | _ -> Error(AD_unsupported_certificate_fatal, perror __SOURCE_FILE__ __LINE__ "Certificate uses unknown signature algorithm or key")
             else
                 Error(AD_bad_certificate_fatal, perror __SOURCE_FILE__ __LINE__ "Certificate is not for signing")
@@ -106,6 +109,15 @@ let get_public_encryption_key (c : cert) : RSAKey.pk Result =
     | None -> Error(AD_decode_error, perror __SOURCE_FILE__ __LINE__ "")
 
 (* ------------------------------------------------------------------------ *)
+
+#if verify
+let get_chain_public_signing_key (chain : chain) (a:Sig.alg) : Sig.pkey Result =
+    failwith "unverified" 
+
+let get_chain_public_encryption_key (chain : chain) : RSAKey.pk Result=
+    failwith "unverified" 
+
+#else
 let get_chain_public_signing_key (chain : chain) a =
     match chain with
     | []     -> Error(AD_internal_error, perror __SOURCE_FILE__ __LINE__ "This is likely a bug, please report it")
@@ -115,6 +127,7 @@ let get_chain_public_encryption_key (chain : chain) =
     match chain with
     | []     -> Error(AD_internal_error, perror __SOURCE_FILE__ __LINE__ "This is likely a bug, please report it")
     | c :: _ -> get_public_encryption_key c
+#endif
 
 (* ------------------------------------------------------------------------ *)
 
@@ -131,13 +144,18 @@ let validate_cert_chain (sigkeyalgs : Sig.alg list) (chain : chain) =
             false
 
 (* ------------------------------------------------------------------------ *)
-let get_hint (chain : chain) =
+#if verify
+let get_hint (chain : chain) : hint option =
+    failwith "unverified" 
+#else
+let get_hint (chain : chain) : hint option =
     match chain_to_x509list chain with
     | Some x509list ->    
         match x509list with
         | []     -> None
         | c :: _ -> Some (get_name_info c) (* FIXME *)
     | None -> None
+#endif
 
 (* ------------------------------------------------------------------------ *)
 let is_chain_for_signing (chain : chain) =
@@ -152,6 +170,13 @@ let consCertificateBytes c a =
     let cert = vlbytes 3 c in
     cert @| a
 
+#if verify
+let certificateListBytes (certs: chain) : bytes = 
+    failwith "unverified" 
+
+let parseCertificateList (toProcess:bytes) (list:chain) : chain Result = 
+    failwith "unverified" 
+#else
 let certificateListBytes certs =
     let unfolded = List.foldBack consCertificateBytes certs empty_bytes in
     vlbytes 3 unfolded
@@ -168,3 +193,4 @@ let rec parseCertificateList toProcess list =
                 let list = list @ [nextCert] in
                 parseCertificateList toProcess list
         else Error(AD_decode_error, perror __SOURCE_FILE__ __LINE__ "")
+#endif
