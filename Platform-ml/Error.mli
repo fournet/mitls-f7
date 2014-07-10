f15ed6ee99ad99a981216d72d20a187df3899d24
Karthikeyan Bhargavan <karthik.bhargavan@gmail.com>
2014-07-10 11:33:07 +0200

    committing

--- Platform-ml/Error.mli
+++ Platform-ml/Error.mli
@@ -1,10 +1,21 @@
-﻿module Error
-
-type ('a,'b) OptResult =
+type ('a,'b) ty_OptResult =
     | Error of 'a
     | Correct of 'b
 
 val perror: string -> string -> string -> string
-val correct: 'a -> ('b,'a) OptResult
+
+(*@ val correct: x:'a -> y:('b,'a) OptResult{y = Correct(x)} *)
+val correct: 'a -> ('b,'a) ty_OptResult
+
+(* Both unexpected and unreachable are aliases for failwith;
+   they indicate code that should never be executed at runtime.
+   This is verified by typing only for the unreachable function;
+   this matters e.g. when dynamic errors are security-critical *)
+
+(*@ val unexpected: string -> 'a {false} @*)
 val unexpected: string -> 'a
-val unreachable: string -> 'a
+
+(*@ val unreachable: string {false} -> 'a @*)
+val unexpected: string -> 'a
+
+
--- Platform-ml/Tcp.mli
+++ Platform-ml/Tcp.mli
@@ -1,30 +1,27 @@
-﻿module Tcp
-
 open Bytes
 open Error
 
-type NetworkStream 
-type TcpListener 
+type ty_NetworkStream 
+type ty_TcpListener 
 
 (* Create a network stream from a given stream.
    Only used by the application interface TLSharp. *)
-
-val create: System.IO.Stream -> NetworkStream
+(* val create: System.IO.Stream -> NetworkStream*)
 
 (* Server side *)
 
-val listen: string -> int -> TcpListener
-val acceptTimeout: int -> TcpListener -> NetworkStream
-val accept: TcpListener -> NetworkStream
-val stop: TcpListener -> unit
+val listen: string -> int -> ty_TcpListener
+val acceptTimeout: int -> ty_TcpListener -> ty_NetworkStream
+val accept: ty_TcpListener -> ty_NetworkStream
+val stop: ty_TcpListener -> unit
 
 (* Client side *)
 
-val connectTimeout: int -> string -> int -> NetworkStream
-val connect: string -> int -> NetworkStream
+val connectTimeout: int -> string -> int -> ty_NetworkStream
+val connect: string -> int -> ty_NetworkStream
 
 (* Input/Output *)
 
-val read: NetworkStream -> int -> (string,bytes) OptResult
-val write: NetworkStream -> bytes -> (string,unit) OptResult
-val close: NetworkStream -> unit
+val read: ty_NetworkStream -> int -> (string,bytes) ty_OptResult
+val write: ty_NetworkStream -> bytes -> (string,unit) ty_OptResult
+val close: ty_NetworkStream -> unit
