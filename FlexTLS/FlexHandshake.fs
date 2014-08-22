faaed2a2a18e44df3512d689bc59366186e32abb
Benjamin Beurdouche <benjamin.beurdouche@inria.fr>
2014-08-22 22:49:31 +0200

    implement sending methods for FlexHandshake

--- FlexTLS/FlexHandshake.fs
+++ FlexTLS/FlexHandshake.fs
@@ -5,15 +5,24 @@ module FlexHandshake
 open Bytes
 open Error
 open TLSConstants
+open HandshakeMessages
 
 open FlexTypes
 open FlexRecord
-
+open FlexConstants
 
 
 
 type FlexHandshake =
     class
+
+    (* Make header bytes for a defined HandshakeType and prepend it to a content *)
+    static member makeHeader (hstype:HandshakeType, ?odata:bytes) : bytes =
+        let data = defaultArg odata empty_bytes in
+        let htb = htBytes hstype in
+        let vldata = vlbytes 3 data in
+        htb @| vldata 
+        
 
     (* Parse a Handshake message. Return message type, payload, to_log -- that is raw header+payload -- and the remainder of the buffer *)
     static member parseHSMessage (buf:bytes) =
@@ -50,5 +59,13 @@ type FlexHandshake =
         | Correct(hst,payload,to_log,rem) ->
                 let st = updateIncomingHSBuffer st rem in
                 (st,hst,payload,to_log)
+
+    (* Send handshake message *)
+    static member send (st:state, hstype:HandshakeType, msgPayload:bytes, ?ofp:fragmentationPolicy) : state =
+        let fp = defaultArg ofp defaultFragmentationPolicy in
+        let payload = FlexHandshake.makeHeader(hstype,msgPayload) in
+        let buf = st.write.hs_buffer @| payload in
+        let st = FlexRecord.updateOutgoingHSBuffer st buf in
+        FlexRecord.send(st,Handshake,fp)
 
     end
