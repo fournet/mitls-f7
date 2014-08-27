aa5b39d2aa1bbc194893b64792da431aa524b1d1
Benjamin Beurdouche <benjamin.beurdouche@inria.fr>
2014-08-27 11:45:40 +0200

    fix FlexCCS message sending by putting it outside of any buffer and putting the proper content type

--- FlexTLS/FlexCCS.fs
+++ FlexTLS/FlexCCS.fs
@@ -11,7 +11,7 @@ open FlexState
 open FlexRecord
 
 
-// FIXME : The state should be updated by the new security context after receiving and sending the CCS
+
 
 type FlexCCS =
     class
@@ -19,7 +19,7 @@ type FlexCCS =
     (* Receive function for handshake ChangeCipherSpecs message *)
     static member receive (st:state, ?nsc:nextSecurityContext) : state * nextSecurityContext * FChangeCipherSpecs =
         let nsc = defaultArg nsc nullNextSecurityContext in
-        let st,payload = FlexRecord.getFragmentContent(st,Handshake,1) in
+        let st,payload = FlexRecord.getFragmentContent(st,Change_cipher_spec,1) in
         let fccs = 
             if payload = HandshakeMessages.CCSBytes then
                 {nullFChangeCipherSpecs with payload = payload }
@@ -31,13 +31,9 @@ type FlexCCS =
     (* Send function for handshake ChangeCipherSpecs message *)
     static member send (st:state, ?nsc:nextSecurityContext, ?fp:fragmentationPolicy) : state * nextSecurityContext * FChangeCipherSpecs =
         let fp = defaultArg fp defaultFragmentationPolicy in
-        let nsc = defaultArg nsc nullNextSecurityContext in
-        let payload = HandshakeMessages.CCSBytes in
-        // TODO : Should we consider CCS part of the Handshake or not ?...
-        (* Here I consider it is inside the handshake so I store it in the HS buffer *)
-        let st = FlexState.updateOutgoingHSBuffer st payload in
-        let st = FlexRecord.send(st,Handshake,fp) in
-        let fccs = {nullFChangeCipherSpecs with payload = payload } in
+        let nsc = defaultArg nsc nullNextSecurityContext in 
+        let st = FlexRecord.send(st,Change_cipher_spec,fp) in
+        let fccs = {nullFChangeCipherSpecs with payload = ccs_buffer } in
         st,nsc,fccs
 
     end
