880603ec9c1b708a549bd0ce213b82a0669fc825
Benjamin Beurdouche <benjamin.beurdouche@inria.fr>
2014-10-06 12:46:59 +0200

    TLS 1.3 Handshake is ready

--- FlexTLS/FlexCertificateVerify.fs
+++ FlexTLS/FlexCertificateVerify.fs
@@ -70,7 +70,15 @@ type FlexCertificateVerify =
                 | Correct(alg,signature) ->
                     if checkLog then
                         (match si.protocol_version with
-                        | TLS_1p3 | TLS_1p2 | TLS_1p1 | TLS_1p0 ->
+                        | TLS_1p3 ->
+                            (match Cert.get_chain_public_signing_key si.serverID alg with
+                            | Error(ad,x) -> failwith x
+                            | Correct(vkey) ->
+                                if Sig.verify alg vkey log signature then
+                                    (alg,signature)
+                                else
+                                    failwith (perror __SOURCE_FILE__ __LINE__ "Signature does not match !"))
+                        | TLS_1p2 | TLS_1p1 | TLS_1p0 ->
                             (match Cert.get_chain_public_signing_key si.clientID alg with
                             | Error(ad,x) -> failwith x
                             | Correct(vkey) ->
