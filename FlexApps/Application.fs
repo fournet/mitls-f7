19ffe5ca62920e7593480be8bcfbcfe13255ffb4
Benjamin Beurdouche <benjamin.beurdouche@inria.fr>
2014-10-22 13:55:05 +0200

    Add Late CCS scenarios to Application.fs

--- FlexApps/Application.fs
+++ FlexApps/Application.fs
@@ -11,6 +11,7 @@ open FlexApps
 open Attack_Alert
 open Attack_FragmentClientHello
 open Attack_EarlyCCS
+open Attack_JavaLateCCS
 open Attack_TripleHandshake
 open Handshake_full_RSA
 open Handshake_full_DHE
@@ -47,6 +48,14 @@ let main argv =
     (* Early CCS attack MITM *)
 //    let sst,cst = Attack_EarlyCCS.runMITM("0.0.0.0","127.0.0.1",4433) in
 //    printf "Early CCS attack finished\n";
+
+    (* Java Late CCS attack as Server *)
+//    let sst,cst = LateCCS.server("0.0.0.0") in
+//    printf "Java Late CCS attack finished\n";
+
+    (* Java Late CCS attack MITM *)
+//    let sst,cst = LateCCS.runMITM("www.inria.fr") in
+//    printf "Java Late CCS attack finished\n";
 
     (* Triple handshake attack MITM *)
 //    let sst,cst = Attack_TripleHandshake.runMITM("0.0.0.0","rsa.cert-01.mitls.org",6666,"127.0.0.1",4433) in
