b7d80004d1ed9f71b369ac268c6ac4858c786adf
Benjamin Beurdouche <benjamin.beurdouche@inria.fr>
2014-11-16 22:21:17 +0100

    add parsing module for cli

---
+++ FlexApps/Parsing.fs
@@ -0,0 +1,155 @@
+#light "off"
+
+module FlexApps.Parsing
+
+open TLSConstants
+
+
+
+
+type ScenarioOpt    = FullHandshake | FullHanshakeANDResumption | Tests
+type RoleOpt        = RoleClient | RoleServer | RoleMITM
+type LogLevelOpt    = LogLevelTrace | LogLevelDebug | LogLevelInfo | LogLevelNone
+type KeyExchangeOpt = KeyExchangeRSA | KeyExchangeDHE
+
+
+type CommandLineOpts = {
+    scenario      : option<ScenarioOpt>;
+    role          : option<RoleOpt>;
+    kex           : option<KeyExchangeOpt>;
+    connect_addr  : option<string>;
+    connect_port  : option<int>;
+    listen_addr   : option<string>;
+    listen_port   : option<int>;
+    min_pv        : option<ProtocolVersion>;
+    verbosity     : option<LogLevelOpt>;
+}
+
+
+
+
+type Parsing =
+    class
+
+    static member innerParseCommandLineOpts parsedArgs args =
+        let banner () =
+            printf "FlexTLS Command Line Interface\n"
+        in
+        let info () = 
+            banner ();
+            printf "\n";
+            printf "  - Version : FlexTLS 0.0.1\n";
+            printf "              November 20, 2014\n";
+            printf "\n";
+            printf "  - Authors : Benjamin Beurdouche & Alfredo Pironti\n";
+            printf "              INRIA Paris-Rocquencourt\n";
+            printf "              Team Prosecco\n";
+            printf "\n";
+            printf "  - Website : http://www.mitls.org\n"
+        in
+        let help () = 
+            banner ();
+            printf "  -i    : Infos about this software\n";
+            printf "\n";
+            printf "  -s     :    Scenario to execute                  (default : FullHandshake)\n";
+            printf "  -r     : *  Role                                 (required)\n";
+            printf "               - Client : {c,C,Client}\n";
+            printf "               - Server : {s,S,Server}\n";
+            printf "               - Both   : {m,M,MITM}\n";
+            printf "  -k     : *  Key exchange                         (required)\n";
+            printf "               - RSA    : {r,rsa,RSA}\n";
+            printf "               - DHE    : {dhe,DHE}\n";
+            printf "               - ECDHE  : {ec,ecdhe,ECDHE}\n";
+            printf "  -pv    : [] Protocol version minimum             (default : TLS1p2)\n";
+            printf "               - SSL 3.0 : {30,ssl3,SSL3}\n";
+            printf "               - TLS 1.0 : {10,tls10,TLS10}\n";
+            printf "               - TLS 1.1 : {11,tls11,TLS11}\n";
+            printf "               - TLS 1.2 : {12,tls12,TLS12}\n";
+            printf "               - TLS 1.3 : {13,tls13,TLS13}\n";
+            printf "  -ca    : [] Connect to address or domain _       (default : localhost)\n";
+            printf "  -cp    : [] Connect to port number _             (default : 443)\n";
+            printf "  -ccert : [] Certificate CN to use if Client _    (default : rsa.mitls.org)\n";
+            printf "  -la    : [] Listening to address or domain _     (default : localhost)\n";
+            printf "  -lp    : [] Listening to port number _           (default : 4433)\n";
+            printf "  -lcert : [] Certificate CN to use if Server      (default : rsa.mitls.org)\n";
+            printf "  -lauth :    Request client authentication\n";
+            printf "  -v     : [] Verbosity                            (default : Info)\n";
+            printf "               - Trace : {3,trace,Trace}\n";
+            printf "               - Debug : {2,debug,Debug}\n";
+            printf "               - Info  : {1,info,Info}\n";
+            printf "               - None  : {0,none,None}\n";
+            printf "\n";
+            printf " *  =>  Require an argument\n";
+            printf " [] =>  Default will be used if not provided\n";
+        in
+
+        // Process options
+        match args with
+        // Infos and Help
+        | []        -> ()
+        | "-h"::t   -> help ()
+
+        // Match valid options
+        | "-s"::t ->
+            (match t with
+            | "FullHandshake"::tt | "fullhandshake"::tt | "fh"::tt | "FH"::tt ->
+                Parsing.innerParseCommandLineOpts {parsedArgs with scenario = Some(FullHandshake)} tt
+            | "Tests"::tt | "tests"::tt ->
+                Parsing.innerParseCommandLineOpts {parsedArgs with scenario = Some(Tests)} tt
+            | _ -> help(); eprintf "ERROR : -s argument is not a valid scenario"
+            )
+
+        | "-r"::t ->
+            (match t with
+            | "Client"::tt | "C"::tt | "c"::tt -> Parsing.innerParseCommandLineOpts {parsedArgs with role = Some(RoleClient)} tt
+            | "Server"::tt | "S"::tt | "s"::tt -> Parsing.innerParseCommandLineOpts {parsedArgs with role = Some(RoleServer)} tt
+            | "MITM"::tt | "M"::tt | "m"::tt -> Parsing.innerParseCommandLineOpts {parsedArgs with role = Some(RoleMITM)} tt
+            | _ -> help(); eprintf "ERROR : -r argument is not a valid role"
+            )
+
+        | "-ca"::t ->
+            (match t with
+            | addr::tt -> Parsing.innerParseCommandLineOpts {parsedArgs with connect_addr = Some(addr)} tt
+            | [] -> help(); eprintf "ERROR : -ca has to be provided either a domain name or an ip address"
+            )
+
+        | "-cp"::t ->
+            (match t with
+            | sport::tt ->
+                let success,port = System.Int32.TryParse sport in
+                if success then
+                    Parsing.innerParseCommandLineOpts {parsedArgs with connect_port = Some(port)} tt
+                else help(); eprintf "ERROR : -cp argument not a correct integer"
+            | [] -> help(); eprintf "ERROR : -cp has to be provided a port number"
+            )
+
+        | "-la"::t ->
+            (match t with
+            | addr::tt -> Parsing.innerParseCommandLineOpts {parsedArgs with listen_addr = Some(addr)} tt
+            | [] -> help(); eprintf "ERROR : -la has to be provided either a domain name or an ip address"
+            )
+
+        | "-lp"::t ->
+            (match t with
+            | sport::tt ->
+                let success,port = System.Int32.TryParse sport in
+                if success then
+                    Parsing.innerParseCommandLineOpts {parsedArgs with connect_port = Some(port)} tt
+                else help(); eprintf "ERROR : -lp argument is not a correct integer"
+            | [] -> help(); eprintf "ERROR : -lp has to be provided a port number"
+            )
+
+        | "-k"::t ->
+            (match t with
+            | "RSA"::t -> Parsing.innerParseCommandLineOpts {parsedArgs with kex = Some(KeyExchangeRSA)} t
+            | "DHE"::t -> Parsing.innerParseCommandLineOpts {parsedArgs with kex = Some(KeyExchangeDHE)} t
+            | "ECDHE"::t -> eprintf "ERROR : -k ECDHE support is in progress ! Be back soon =)"
+            | h::t -> help(); eprintf "ERROR : -k argument is not a valid key exchange mechanism"
+            )
+
+        | "-i"::t -> info ()
+        
+        // Invalid command
+        | h::t      -> help(); eprintf "Error : %A is not a valid option !\n" h
+
+    end
